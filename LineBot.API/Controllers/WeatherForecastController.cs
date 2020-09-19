cb1a0859b2d47f5b21cd2b3759d43b878802b552
Jason Chen <jason.chen@titansoft.com.sg>
2020-09-19 16:26:34 +0800

    push line access token

--- LineBot.API/Controllers/WeatherForecastController.cs
+++ LineBot.API/Controllers/WeatherForecastController.cs
@@ -1,9 +1,10 @@
-﻿using System;
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Mvc;
-using Microsoft.Extensions.Logging;
+using System.Net.Http;
+using System.Net.Http.Headers;
 
 namespace LineBot.API.Controllers
 {
@@ -34,6 +35,19 @@ namespace LineBot.API.Controllers
                 Summary = Summaries[rng.Next(Summaries.Length)]
             })
             .ToArray();
+        }
+
+        [HttpGet]
+        public ActionResult Test()
+        {
+            var httpClient = new HttpClient();
+
+            httpClient.DefaultRequestHeaders
+                      .Add("Authorization", @"Bearer {YvWZ+lhw4mBszIb4cuow/211m0BvEDX2AB5auS3IwkzkN1POJ3ej4Gw20tX17/hL/3PN0wKXlHl4xeFcySt4TbhujKQWqUlcuJhnIeI84xb3kHagP3gIwEoHppOvugwexZeNAPc14O0fbX7Stek35QdB04t89/1O/w1cDnyilFU=}");
+
+            var postData = "";
+
+            return Ok("OK");
         }
     }
 }
